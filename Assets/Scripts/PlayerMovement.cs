ec2cbf323e58a86bb4e3710f2053dd2061df83e4
VinylKnight <roger.bucio@loop.colum.edu>
2018-10-20 11:48:17 -0500

    movement implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private Rigidbody PlayerRigidBody;
+    [SerializeField]
+    private float movementSpeed = 10;
+    void start()
+    {
+        PlayerRigidBody = GetComponent<Rigidbody>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        PlayerRigidBody.AddForce(movement * movementSpeed);
+
+
+    }
 }
